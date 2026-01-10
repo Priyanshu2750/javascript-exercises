8782d512cc991b7284c8630d834e1d8197734dd4
Priyanshu Barnwal <pbarnwal275@gmail.com>
2026-01-10 17:18:04 +0530

    Solve the seventh exercise
    Reverse the string given

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let ans = "";
+  for (let char of word) {
+    ans = char + ans;
+  }
+  return ans;
 };
 
 // Do not edit below this line
