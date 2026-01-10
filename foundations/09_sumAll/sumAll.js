9c011a5f89bd7287942fa7584a4653c18eb2aab0
Priyanshu Barnwal <pbarnwal275@gmail.com>
2026-01-10 21:27:45 +0530

    Solve the ninth exercise
    Sum the elements from the starting to the ending number

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let sum = 0;
+  if (typeof start != "number" || typeof end != "number") return "ERROR";
+  if (start < 0 || end < 0) return "ERROR";
+  if (Math.ceil(start) != start || Math.ceil(end) != end) return "ERROR";
+  for (let i = Math.min(start, end); i <= Math.max(start, end); i++) sum += i;
+  return sum;
 };
 
 // Do not edit below this line
