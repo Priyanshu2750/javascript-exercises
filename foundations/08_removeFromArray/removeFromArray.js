aa6ab23ebd50d521809b39dd049df8deb743befb
Priyanshu Barnwal <pbarnwal275@gmail.com>
2026-01-10 18:29:09 +0530

    Solve the eighth exercise
    Remove elements input by the user from the array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...rest) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (rest.includes(arr[i]) == false) newArr.push(arr[i]);
+  }
+  return newArr;
 };
 
 // Do not edit below this line
